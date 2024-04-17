30f1dec70ec290dcf28b05ed41c7abb9a80eb6c4
TrungNg <ntrung1121@gmail.com>
2024-04-17 22:26:11 +0700

    Route Product and ProductInfo pages

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import {
+    BarsOutlined,
+    LogoutOutlined,
+    SettingOutlined,
+    ShoppingCartOutlined,
+  } from "@ant-design/icons";
+import { Role } from "@/constants/role";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
